399d1e667ec7a6f1e291181a52304a1d76569f12
1B10DK <114579229+Dek0sio@users.noreply.github.com>
2022-10-05 11:17:47 +0000

    Zrobiono 3 zadania z karty pracy 2

---
+++ kartapracy2.py
@@ -0,0 +1,22 @@
+# zad 1
+
+# a = int(input("PODAJ LICZBE "))
+# if a % 3 == 0:
+#     print("TAK")
+# else:
+#     print("NIE")
+
+# zad 2
+
+# a = int(input("DAWAJ LICZBE"))
+# if a>=100 and a<1000 and a % 17==0:
+#       print ("TAK")
+# else: 
+#       print ("NIE")
+
+# zad 3
+# wiek=int(input("A ILE TY MASZ LAT?"))
+# if wiek >= 18:
+#         print("tak")
+# else:
+#         print("nie")
