b4524f2b87758be14254d64840663cd09e024061
1B10DK <114579229+Dek0sio@users.noreply.github.com>
2022-10-12 11:19:52 +0000

    skonczono zad 1 z karty pracy 3

---
+++ kartapracy3.py
@@ -0,0 +1,18 @@
+#pre
+# for i in range(10,22): print(i,end=" ")
+
+# for i in range(15,32,2): print(i,end=" ")
+
+# for i in range(99,9,-1): print(i, end=" ")
+
+# for i in range(-99,-9,1):print(i*(-1),end=" ")
+
+# for i in range(100,1000,20): print(i, end=" ")
+
+# zad 1
+# n = int(input())
+# for i in range(n):
+#     print(i**3 + 3, end=" ")
+
+#zad 2
+for i in range(105,1000,15)
