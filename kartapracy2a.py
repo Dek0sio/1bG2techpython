a85556afe71bdad22672c58e77a93c1e36f37a58
1B10DK <114579229+Dek0sio@users.noreply.github.com>
2022-10-16 14:17:13 +0000

    Ukończono kartapracy2.py

---
+++ kartapracy2a.py
@@ -0,0 +1,64 @@
+# zad 1
+# print("podaj liczbe ")
+# a,b = int(input()), int(input())
+# if (a + b) / 2:
+#   print("nie")
+# else:
+#   print("tak")
+
+
+
+# zad 2
+# a,g = int(input()), int(input())
+# if (a + g) / 2 > (a**g)/(1/2):
+#   print("tak")
+# else:
+#   print("nie")
+
+
+
+# zad 3
+# k,l,m = int(input()), int(input()), int(input())
+# if k==l:
+#   print("tak")
+#   print("k,l")
+# if l==m:
+#   print("tak")
+#   print("l,m")
+# if m==k:
+#   print("tak")
+#   print("m,k")
+
+
+
+# zad 4
+# a,b,c,d = int(input()), int(input()), int(input()), int(input())
+# print(min(a,b,c,d))
+
+
+
+# zad 5
+# a, b, c = int(input()), int(input()), int(input())
+# if a<(a+b) and a<(b-c) and b<(a+c) and b<(a-c) and c<(a+b) and c<(a-b):
+#   print("tak")
+# else:
+#   print("nie")
+
+
+
+# zad 6
+# a, b, c = int(input()), int(input()), int(input())
+# if a**2 + b**2 == c**2:
+#   print("prostokątny")
+# else:
+#   print(" ")
+
+# if a**2 + b**2 < c**2:
+#   print("rozwartokątny")
+# else:
+#   print(" ")
+
+# if a**2 + b**2 > c**2:
+#   print("ostrokątny")   
+# else:
+#   print(" ")
