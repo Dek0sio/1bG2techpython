0f5ccd644f357bdb27a2a8e94bc9b9b50df96305
1B10DK <114579229+Dek0sio@users.noreply.github.com>
2022-11-01 15:36:42 +0000

    zakonczono karte pracy 3b

--- kartapracy3.py
+++ kartapracy3.py
@@ -15,4 +15,50 @@
 #     print(i**3 + 3, end=" ")
 
 #zad 2
-for i in range(105,1000,15)
+# for i in range(105,1000,15)
+
+# PRE 2
+# petłe for liczb trzycyfrowych podzielonych przez 13
+# for i in range(100,1000):
+#     if i% 13 == 0:
+#         print(i)
+# petle for liczb dwucofrowych parzystych
+# for i in range(10,99,2): print(i, end = " ")
+# petle for poteg cyfr: 0,1,4,9,16....81
+# for i in range(10):
+#     print(i*i, end=" ")
+
+# zad 3
+# p = int(input())
+# for i in range(1, n+1)
+
+# zad 4
+# suma = 0
+# for i in range(10, 100):
+#     suma = suma + i
+# print(suma)
+
+# SUMA LICZB TRZYCYFROWYCH PARZYSTYCH
+# suma = 0
+# for i in range(100,1000,2)
+# suma = suma + i
+# print(suma)
+
+# zad 5
+# n = int(input())
+# suma = n * (n+1) // 2
+
+# for i in range(n-1):
+#     k = int(input())
+#     suma = suma - k
+
+# print(suma)
+# zad 6
+# n = int(input())
+# a, b = 0, 1
+
+# for i in range(n):
+#     temp = a
+#     a = b
+#     b = temp + b
+#     print(a, end=" ")
