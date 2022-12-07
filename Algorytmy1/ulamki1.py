0a99336fe62aa8d3bdd45886130d7fad63e6c924
1B10DK <114579229+Dek0sio@users.noreply.github.com>
2022-12-07 12:19:58 +0000

    ulamki1 dodane

---
+++ Algorytmy1/ulamki1.py
@@ -0,0 +1,15 @@
+# a = int(input("podaj licznik 1 "))
+# b = int(input("podaj mianownik 1 "))
+# c = int(input("podaj licznik 2 "))
+# d = int(input("podaj mianownik 2 "))
+# x, y = b, d
+
+# ilocz = x * y
+# while y > 0:
+#     x,y = y,x % y
+# nww = ilocz // x
+
+# e = (nww // b) * a
+# f = (nww // d) * c
+# g = e + f
+# print(f"{a}/{b} + {c}/{d} = {e}/{nww} + {f}/{nww} = {g}/{nww}")
---
+++ cezar/pre.py
@@ -0,0 +1,31 @@
+# Funkcja ord() - zwraca kod ascii danego znaku
+# print(ord("A"))
+# print(ord("B"))
+# print(ord("Z"))
+
+# Funkcja chr() - zwraca znak danego kodu ascii
+# print(chr(69))
+# print(chr(60))
+# print(chr(65))
+# znaki ASCII od A-Z maja kody 65-90 !!!!!!
+
+
+# wypisz petla for cały alfabet
+# for i in range(65, 91):
+#     print(chr(i))
+
+# jak słowo rozbic na litery?
+
+# napis = "POLSKA"
+# print(len(napis)) # zwraca dlugosc napisu
+# print(napis[0])   # napis to lista
+
+# wypiszcie kody ASCII literek napisu w petli for
+# napis = "POLSKA"
+# for i in range(len(napis)):
+    # print(ord(napis[i]))
+
+# napis = "POLSKA"
+# for i in range(len(napis)):
+#     print(chr(ord(napis[i] +3 ))
+
