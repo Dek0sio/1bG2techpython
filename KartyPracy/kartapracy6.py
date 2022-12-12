95ae8fbca2eeb69312c972658adfc0a2125f5bc9
1B10DK <114579229+Dek0sio@users.noreply.github.com>
2022-12-12 12:17:00 +0000

    dodano karty pracy od 4 do 6a

---
+++ KartyPracy/kartapracy6.py
@@ -0,0 +1,28 @@
+# zad 1
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if 
+
+
+# zad 2
+# suma = 0
+# for i in range(100, 1000):
+#     if i % 8 == 0 and i % 16 != 0:
+#         suma = suma + i
+# print(suma)
+
+# zad 3
+
+# for i in range(99,9,-1):
+#     if i % 7 == 0:
+#         podzielnik = i
+#         break
+        
+# print(podzielnik)
+
+# ilosc = 0
+# for i in range(1000, 10000):
+#     if i % podzielnik == 0:
+#         ilosc += 1
+# print(ilosc)
