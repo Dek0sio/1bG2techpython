1ac518e6887123faf194e52a8631d4d8fecc3562
1B10DK <114579229+Dek0sio@users.noreply.github.com>
2022-12-20 07:48:32 +0000

    chuj chuj chuj chuj chuj chuj chuj chuj chuj chuj chuj chuj chuj chuj v chuj chuj chuj chuj chuj chujchuj chuj chuj chuj chuj chuj chuj chuj chuj chuj v

---
+++ KartyPracy/uczeniesie2.py
@@ -0,0 +1,544 @@
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+ chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
+chuj chuj
+chuj chuj
+chuj chujchuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+chuj chuj
+v
